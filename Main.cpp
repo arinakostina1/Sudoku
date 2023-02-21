16d4a69b179bcac03390bf984e63efbf83479974
arinakostina-exe <119154676+arinakostina-exe@users.noreply.github.com>
2023-02-21 17:32:13 +0200

    commit

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,22 @@
+/*
+
+Sudoku
+
+Copyright © 21/02/2023 arinakostina-exe
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+
 // The framerate management, which is between these lines-->     // { -------------------- }
 // is from https://github.com/Kofybrek/Tetris/blob/Main/Source/Main.cpp
 // Maybe there is a slight decrease in cpu consumption, but even without these lines of code the program works fine
